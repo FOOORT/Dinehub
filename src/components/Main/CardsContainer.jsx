7397d8810296408de87b3b80e0c0a8f6a96d1a66
Reconfort Daniel <reconfortdanny@gmail.com>
2023-09-13 10:23:50 +0200

    Add menu

--- src/components/Main/CardsContainer.jsx
+++ src/components/Main/CardsContainer.jsx
@@ -4,7 +4,7 @@ import { Asset } from "../Asset";
 
 const CardsContainer = () => {
   return (
-    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-2">
       <Cards image={Asset.logo} name="Villa" location="South, Huye" />
       <Cards image={Asset.Image1} name="Obinasom" location="South, Huye" />
       <Cards image={Asset.Image2} name="E-Bis" location="South, Huye" />
--- src/components/Menu/cardcontainer.jsx
+++ src/components/Menu/cardcontainer.jsx
@@ -3,7 +3,7 @@ import Cards from "../common/Menu/Cards";
 
 const CardContainer = () => {
   return (
-    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-2">
       <Cards />
       <Cards />
       <Cards />
