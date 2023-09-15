f77e8154acc11cf4d225d61268967c274980498f
Reconfort Daniel <reconfortdanny@gmail.com>
2023-09-15 17:07:21 +0200

    Adding User's skelton (incomplete)

--- src/app/dashboard/user/renewal/page.jsx
+++ src/app/dashboard/user/renewal/page.jsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Main/Header";
 import React from "react";
 import Layout from "../Layout";
-import RenewalLayout from "@/components/Dashboard/user/renewal/renewalLayout";
+import RenewalLayout from "@/components/Dashboard/user/renewal/renewallayout";
 import RenewalOption from "@/components/Dashboard/user/renewal/renewaloption";
 
 const page = () => {
