d4ec9176b1fa6437705a8b526eba459a4dc957c9
Reconfort Daniel <reconfortdanny@gmail.com>
2023-10-24 12:21:09 +0200

    change manager page

--- src/components/common/Dashboard/admin/statscontainer.jsx
+++ src/components/common/Dashboard/admin/statscontainer.jsx
@@ -10,7 +10,8 @@ const StatsContainer = ({ Stats }) => {
   // }, [Stats]);
 
   // console.log("Stats: ", statsData);
-  console.log("Stats: ", Stats);
+
+  // console.log("Stats: ", Stats);
   return (
     <div className="w-full grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 mt-4 gap-4  bg-">
       {Stats &&
