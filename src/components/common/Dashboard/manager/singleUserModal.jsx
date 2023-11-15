a8e220c1455485802ef4450e3fbdc5bffb74d16d
Reconfort Daniel <reconfortdanny@gmail.com>
2023-11-15 08:20:21 +0200

    add single user data

--- src/components/common/Dashboard/manager/singleUserModal.jsx
+++ src/components/common/Dashboard/manager/singleUserModal.jsx
@@ -3,95 +3,48 @@
 import ActionButton from "@/components/common/actionbutton";
 import React, { useState } from "react";
 import { BiCamera } from "react-icons/bi";
-import { FaTimes, FaStar } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
+import { FiPlus } from "react-icons/fi";
+import { CiLocationOn } from "react-icons/ci";
+import StatsContainer from "../admin/statscontainer";
+import UserStats from "@/components/json/manager/userStats";
+import HistoryTable from "../user/report/common/historytable";
 
 const SingleUserModal = ({ closeModal }) => {
   return (
     <div className="w-screen h-screen fixed left-0 top-0 z-[60] bg-black/50 backdrop-blur-sm flex justify-center items-center">
-      <div className="bg-white p-4 rounded-xl w-5/6 xl:w-4/6 shadow-2xl shadow-slate-400 flex flex-col justify-center items-center">
-        <div className="w-full flex justify-end items-center">
+      <div className="bg-white p-0 rounded-3xl w-5/6 xl:w-4/6 shadow-2xl shadow-slate-400 flex flex-col justify-center items-center duration-100">
+        <div className="w-full flex justify-end items-center p-4">
           <button
-            className="bg-black text-white p-3 rounded-xl relative z-20"
+            className="bg-slate-300 hover:bg-black duration-150 text-white p-3 rounded-full relative z-20"
             onClick={() => closeModal()}
           >
             <FaTimes className="text-lg" />
           </button>
         </div>
-        <div className="flex w-full rounded-lg -mt-10 gap-4 items-center mb-5">
-          <div className=" w-2/5 h-56 border rounded-lg justify-center items-center flex ">
-            <input type="file" className="w-full h-full hidden" />
-            <BiCamera />
-          </div>
-
-          <div className="w-3/5 rounded-lg justify-start items-start flex flex-col gap-2 p-4">
-            <h1 className="text-xl">Business name: Obinasom</h1>
-            <p className=" font-medium text-blue-600 text-sm">
-              obina@gmail.com
-            </p>
-            <div className="p-4 rounded-lg">
-              <p>User one details is like this</p>
-              <div className="flex justify-between items-center mt-2">
-                <h2 className=" text-blue-600 text-sm font-semibold">
-                  222 reviews
-                </h2>
-                <div className="flex items-center gap-2 mt-4">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </div>
-              </div>
+        <div className="flex w-full -mt-16 gap-4 items-center">
+          <div className="flex gap-4 p-4 border-b w-full border-slate-100 items-end justify-between">
+            <div className=" w-36 h-36 border rounded-full justify-center items-center flex ">
+              <input type="file" className="w-full h-full hidden" />
+              <BiCamera />
             </div>
+            <div className="p-4 flex flex-col gap-2 justify-start items-start w-4/6">
+              <h2 className="text-xl font-bold">Reconfort daniel</h2>
+              <div className="flex gap-3 mt-2 text-sm">
+                <CiLocationOn className="text-blue-600" />
+                <p className=" text-base">Kacyiru, Rwanda - </p>
+                {/* now time */}
+                <p>07:51am</p>
+              </div>
+              <p className="text-slate-700">Active since : 30th July 2023 </p>
+            </div>
+            <ActionButton name="Order now" icon={<FiPlus />} className="py-3" />
           </div>
         </div>
-        <div className="bg-slate-100 w-3/5 flex gap-2 justify-evenly items-center rounded-2xl relative z-10 p-4 ">
-          <div className="flex flex-col items-center">
-            <h3 className="text-sm">Owner Firstname</h3>
-            <h1 className="text-lg">Reconfort</h1>
-          </div>
-          <div className="flex flex-col items-center">
-            <h3 className="text-sm">Owner Lastname</h3>
-            <h1 className="text-lg">Daniel</h1>
-          </div>
-          <div className="flex flex-col items-center">
-            <h3 className="text-sm">Owner Username</h3>
-            <h1 className="text-lg lowercase">@daniel</h1>
-          </div>
-        </div>
-        <div className="flex w-full shadow shadow-slate-100 rounded-lg -mt-10 pt-16 gap-4 items-start bg-slate-50 p-4">
-          <div className="w-2/5 flex flex-col justify-start items-start gap-2">
-            <h1 className="text-xl">3 Employees</h1>
-            <ul className="w-full">
-              <li className="p-4 rounded-lg hover:bg-slate-100 w-full duration-150">
-                John doe
-              </li>
-              <li className="p-4 rounded-lg hover:bg-slate-100 w-full duration-150">
-                John doe
-              </li>
-              <li className="p-4 rounded-lg hover:bg-slate-100 w-full duration-150">
-                John doe
-              </li>
-            </ul>
-          </div>
-
-          <ul className="w-3/5 rounded-lg justify-start items-start flex flex-col gap-2 p-4 border">
-            <li className="flex gap-3 items-center w-full hover:bg-slate-100 duration-150 p-4">
-              <h1>Business adress:</h1>
-              <h1>kg 655 st</h1>
-            </li>
-            <li className="flex gap-3 items-center w-full hover:bg-slate-100 duration-150 p-4">
-              <h1>Business adress:</h1>
-              <h1>kg 655 st</h1>
-            </li>
-            <li className="flex gap-3 items-center w-full hover:bg-slate-100 duration-150 p-4">
-              <h1>Business adress:</h1>
-              <h1>kg 655 st</h1>
-            </li>
-            <li className="flex gap-3 items-center w-full hover:bg-slate-100 duration-150 p-4">
-              <h1>Business adress:</h1>
-              <h1>kg 655 st</h1>
-            </li>
-          </ul>
+        <div className="bg-slate-50 p-4 w-full rounded-b-3xl">
+          <StatsContainer Stats={UserStats} />
+          <h3 className="mt-4 -mb-3 text-lg font-semibold">Recent data</h3>
+          <HistoryTable />
         </div>
       </div>
     </div>
